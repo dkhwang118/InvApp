f0b2af32b8f6529abdd02405cc43fb067b2244c7
dkhwang118 <dkhwang@ksu.edu>
2019-10-31 10:40:19 -0500

    Arranged basic MVC framework

---
+++ Controller/controller_main.py
@@ -0,0 +1,27 @@
+###########################################################################
+#
+#   controller_main.py
+#   Original design framework by "101" and "Roberto Leinardi" (https://stackoverflow.com/a/26699122)
+#   Edited by: David K. Hwang
+#
+#
+############################################################################
+
+from PyQt5.QtCore import QObject, pyqtSlot
+
+
+class MainController(QObject):
+    def __init__(self, model):
+        super().__init__()
+
+        self._model = model
+
+    @pyqtSlot(int)
+    def change_amount(self, value):
+        self._model.amount = value
+
+        # calculate even or odd
+        self._model.even_odd = 'odd' if value % 2 else 'even'
+
+        # calculate button enabled state
+        self._model.enable_reset = True if value else False
---
+++ app_main.py
@@ -0,0 +1,41 @@
+###########################################################################
+#
+#   app_main.py
+#   Original design framework by "101" and "Roberto Leinardi" (https://stackoverflow.com/a/26699122)
+#   Edited by: David K. Hwang
+#
+#
+############################################################################
+
+import sys
+from PyQt5.QtWidgets import QApplication
+from model.model import Model
+from controllers.main_ctrl import MainController
+from views.main_view import MainView
+
+############################################
+#   Qt Initialization
+############################################
+
+# command-line args to application
+args = []
+
+# init Qt App w/ args
+app = QApplication(args)
+
+# set Qt app style to 'Fusion'
+app.setStyle('Fusion')
+
+# main application class that connects the MVC framework
+class App(QApplication):
+    def __init__(self, sys_argv):
+        super(App, self).__init__(sys_argv)
+        self.model = Model()
+        self.main_controller = MainController(self.model)
+        self.main_view = MainView(self.model, self.main_controller)
+        self.main_view.show()
+
+
+if __name__ == '__main__':
+    app = App(sys.argv)
+    sys.exit(app.exec_())
